e0124b13ce5b1f29b01ad3853b60c4744d5211f4
quidmonkey <quidmonkey@gmail.com>
2017-07-31 15:00:40 -0400

    Fixed comment parsing to allow for correct list of objects jsDocs.
    jsDoc list of objects take the form object[].prop when documenting properties. Previously, the parseComments function was not accounting for the square brackets. That has been fixed.

--- examples/simple.js
+++ examples/simple.js
@@ -1,9 +1,9 @@
 /**
  * Get a list of full names
- * @param  {Object[]} people          2d array of people
- * @param  {string} people.firstName  First name of a person
- * @param  {string} people.lastName   Last name of a person
- * @return {string[]}                 List of full names
+ * @param  {Object[]} people            2d array of people
+ * @param  {string} people[].firstName  First name of a person
+ * @param  {string} people[].lastName   Last name of a person
+ * @return {string[]}                   List of full names
  */
 module.exports.getFullNames = function getFullNames(people) {
   return people.map(function(person) {
